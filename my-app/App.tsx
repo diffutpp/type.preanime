d592a72d8c6cc7d3fd2be8bd545e6c5d4578280d
diffu.tpp <kisk0807@icloud.com>
2022-11-16 10:01:46 +0900

    ver0.1

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
+import { useState } from "react";
+
+const Colors = {
+  dark: {
+    background: "#E1E1E1",
+    circle: "#252525",
+    text: "F8F8F8",
+  },
+  light: {
+    background: "F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256,0,256,0.2)",
+  false: "rgba(0,0,0,0.1)",
+};
+
+type Theme = "light" | "dark";
 
 export default function App() {
+  const [theme, setTheme] = useState<Theme>("light");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <StatusBar style="auto" />
+      <Switch
+        value={theme === "dark"}
+        onValueChange={(toggled) => {
+          setTheme(toggled ? "dark" : "light");
+        }}
+        trackColor={SWITCH_TRACK_COLOR}
+      />
     </View>
   );
 }
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
